fc5d1c9fc6cd484f75243ea7433993d213921edf
KrisAllan <allanasis666@gmail.com>
2018-11-06 19:21:56 +0800

    signup

--- ems_proj/settings.py
+++ ems_proj/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'events',
-    'crispy_forms'
+    'crispy_forms',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+AUTH_USER_MODEL = 'users.Participant'
